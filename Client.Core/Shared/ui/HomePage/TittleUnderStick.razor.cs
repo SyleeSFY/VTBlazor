c070df10087620318a1c94fa3ec73e3e3f16be9e
Artem <sylees@yandex.ru>
2026-02-11 00:39:23 +0300

    готовый дисциплины

--- Client.Core/Shared/ui/HomePage/TittleUnderStick.razor.cs
+++ Client.Core/Shared/ui/HomePage/TittleUnderStick.razor.cs
@@ -7,6 +7,7 @@ namespace Client.Core.Shared.ui.HomePage
     public partial class TittleUnderStick : TittleBase
     {
         [Parameter] [Required] public string Tittle { get; set; }
+        [Parameter] public bool State { get; set; } = true;
         [Parameter] public int FontSize { get; set; } = 48;
         [Parameter] public int PaddinLeft { get; set; } = 5;
     }
