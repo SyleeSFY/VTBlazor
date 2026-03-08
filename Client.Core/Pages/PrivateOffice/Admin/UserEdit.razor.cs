2693434a134be84a5c7a6b8ab226affaf3804f76
Artem <sylees@yandex.ru>
2026-03-09 01:51:19 +0300

    edit

--- Client.Core/Pages/PrivateOffice/Admin/UserEdit.razor.cs
+++ Client.Core/Pages/PrivateOffice/Admin/UserEdit.razor.cs
@@ -1,4 +1,5 @@
-﻿using Client.Core.Entities.Models.User;
+﻿using Client.Core.Entities.Enums;
+using Client.Core.Entities.Models.User;
 using Microsoft.AspNetCore.Components;
 using System.Net.Http.Json;
 
@@ -8,18 +9,20 @@ namespace Client.Core.Pages.PrivateOffice.Admin
     {
         [Parameter]
         public required int UserId { get; set; }
+
         [Parameter]
         public required int RoleEdit { get; set; }
         private User? _user { get; set; }
 
+        private Role _role;
+
         protected override async Task OnInitializedAsync()
         {
+            _role = (Role)RoleEdit;
             _user = await GetUser(UserId);
         }
 
         private async Task<User> GetUser(int userId)
-        {
-            return await Http.GetFromJsonAsync<User>($"api/User/GetUser/{userId}");
-        }
+            => await Http.GetFromJsonAsync<User>($"api/User/GetUser/{userId}");
     }
 }
